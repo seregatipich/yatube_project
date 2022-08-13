15fe87cf0955af14c60e9ff99c32aece13e71aba
Sergei Poluektov <seregatipich@outlook.com>
2022-08-13 14:22:14 +0200

    added posts and group apps

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'group.apps.GroupConfig',
+    'posts.apps.PostsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
