42a551d9e8a812c13b7409d3d30ddeeef87ee92f
Sergei Poluektov <seregatipich@outlook.com>
2022-08-18 16:01:32 +0200

    recreated templates folder

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    template = 'base.html'
+    template = 'posts/index.html'
     return render(request, template) 
 
 def group_posts(request, slug):
