6c440c2d9d95bb89b8280448eb442461a261445d
Sergei Poluektov <seregatipich@outlook.com>
2022-08-20 16:21:39 +0200

    Not working...

--- yatube/posts/admin.py
+++ yatube/posts/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Group
 
 
 class PostAdmin(admin.ModelAdmin):
@@ -10,3 +10,20 @@ class PostAdmin(admin.ModelAdmin):
 
 
 admin.site.register(Post, PostAdmin)
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = (
+        'pk',
+        'text',
+        'pub_date',
+        'author',
+        'group',
+    )
+    list_editable = ('group',)
+    search_fields = ('text',)
+    list_filter = ('pub_date',)
+    empty_value_display = '-пусто-'
+
+
+admin.site.register(Group)
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,16 +1,20 @@
-from multiprocessing import context
-from django.http import HttpResponse
-from django.shortcuts import render
-from .models import Post
+from django.shortcuts import render, get_object_or_404
+from .models import Post, Group
 
 
 def index(request):
-    post = Post.objects.order_by('-pub_date')[:10]
+    posts = Post.objects.order_by('-pub_date')[:10]
     context = {
-        'posts':posts
+        'posts': posts,
     }
     return render(request, 'posts/index.html', context)
 
 
 def group_posts(request, slug):
-    return HttpResponse(f'Hi {slug}')
+    group = get_object_or_404(Group, slug=slug)
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
+    context = {
+        'group': group,
+        'posts': posts,
+    }
+    return render(request, 'posts/group_list.html', context)
