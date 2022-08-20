36d3dae862be88228d30cf5d8a69fc8217b5d90a
Sergei Poluektov <seregatipich@outlook.com>
2022-08-20 13:58:02 +0200

    added admin panel for posts app

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,11 +1,16 @@
+from multiprocessing import context
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Post
 
-# Create your views here.
 
 def index(request):
-    template = 'posts/index.html'
-    return render(request, template) 
+    post = Post.objects.order_by('-pub_date')[:10]
+    context = {
+        'posts':posts
+    }
+    return render(request, 'posts/index.html', context)
+
 
 def group_posts(request, slug):
     return HttpResponse(f'Hi {slug}')
